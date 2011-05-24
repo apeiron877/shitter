66ed1278346503626f10b2b569213e27e64a2482
Dominick Reinhold <d.reinhold@yahoo.com>
2011-05-23 21:14:13 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shitter::Application.config.secret_token = 'e8ed7a013ac870880931b58f0c904965d958fb9872691afeee8625c60a2e7042772bfb10cf7a1cd2dddad26f951e7f6a3cb0237453c566f74f53d891cd14adbb'
