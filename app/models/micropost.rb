2f7d1a582220742d23c3b186268fc0cff291d3c5
Dominick Reinhold <d.reinhold@yahoo.com>
2011-05-24 21:07:11 -0500

    Added Static Pages

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
+  belongs_to :user
   validates :content, :length => { :maximum => 140 }
   attr_accessible :user_id, :content
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Shitter::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
   resources :microposts
 
   devise_for :users
@@ -52,7 +58,7 @@ Shitter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "microposts#index"
+   root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
